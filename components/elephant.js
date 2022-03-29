01ed4437842c7fff66b3c8237f7ab54d818e3798
Xay123 <zacharylayland10@gmail.com>
2022-03-28 23:56:38 -0500

    added gitattributes

--- components/elephant.js
+++ components/elephant.js
@@ -3,7 +3,7 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { loadGLTFModel } from '../lib/model'
 import { ElephantSpinner, ElephantContainer } from './elephant-loader'
-import ElephantGLB from '/elephant.glb'
+import ElephantGLB from '../public/elephant.glb'
 
 function easeOutCirc(x) {
   return Math.sqrt(1 - Math.pow(x - 1, 4))
