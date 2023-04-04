e6e409d9ae3d3df19763aee597d53540342ee7de
Zachary Layland <65696050+Xay123@users.noreply.github.com>
2023-04-04 09:00:47 -0500

    Delete executer.js

--- executer.js
+++
@@ -1,9 +0,0 @@
-const Executer = () => (
-    <Layout title="Executer">
-        <Container mt={20}>
-            <Heading as="h4" fontSize={20} mb={4}>
-                Popular Posts
-            </Heading>
-        </Container>
-    </Layout>
-)
