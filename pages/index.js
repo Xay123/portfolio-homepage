0d068e93c0a681e48572f610a66ab2e7ff96b5e9
Zachary Layland <65696050+Xay123@users.noreply.github.com>
2023-04-04 08:47:35 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -56,7 +56,7 @@ const Page = ( ) => {
 
         <Section delay={0.3}>
             <Heading as="h3" variant="section-title">
-                I ❤
+                I Like
             </Heading>
             <Paragraph>
                 Music, {' '}
