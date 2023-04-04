055668f7b46b975efedacedcfcfb86b28fdc7136
Zachary Layland <65696050+Xay123@users.noreply.github.com>
2023-04-04 09:05:47 -0500

    Update executer.js

--- pages/executer.js
+++ pages/executer.js
@@ -1,3 +1,8 @@
+import { Container, Heading, SimpleGrid} from "@chakra-ui/react";
+import Layout from '../components/layouts/article'
+import Section from "../components/section";
+import { WorkGridItem } from "../components/grid-item";
+
 const Executer = () => (
     <Layout title="Executer">
         <Container mt={20}>
